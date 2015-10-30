015c05439a5ae5e261d0d41222d9477a841bf3aa
jalcara1 <jalcara1@a8a942dd-ee53-4a0d-a99f-0cee3f59fc4b>
2015-10-30 18:28:02 +0000

    ejemplos de ruby

---
+++ seguimiento/clase28/echo.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/ruby
+#Esto es un comentario
+
+#Inicio del programa
+s = gets
+puts s
---
+++ seguimiento/clase28/funciones.rb
@@ -0,0 +1,17 @@
+#Definicion de una funcion
+def echo()
+  s = gets
+  puts s
+end
+
+def echo2()
+  s = gets
+  puts s
+  s
+end
+
+def echo3()
+  s = gets
+  puts s
+  return s
+end
