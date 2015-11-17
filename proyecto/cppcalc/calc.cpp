2ba2c7d895b4151b3651a3c40e205bf48d826236
jalcara1 <jalcara1@a8a942dd-ee53-4a0d-a99f-0cee3f59fc4b>
2015-11-17 22:00:00 +0000

    practica01

--- proyecto/cppcalc/calc.cpp
+++ proyecto/cppcalc/calc.cpp
@@ -16,56 +16,59 @@ int main(int argc, char* argv[]){
   string line;
   int result;
   calc = new Calculator();
-  
-  while(cont < argc){
+
+  if(argc > 1){  
+    while(cont < argc){
     
-    char buffer[MAXBUF];
-    ifstream filein(argv[cont]);//Fichero de entrada
+      char buffer[MAXBUF];
+      ifstream filein(argv[cont]);//Fichero de entrada
 
-    while (filein.getline(buffer,MAXBUF)) {
+      while (filein.getline(buffer,MAXBUF)) {
+	try{
+	  string buffer2(buffer);
+	  istringstream ins(buffer2);
+
+	  string valorLinea;
+	  ins >> valorLinea;
+
+	  result = calc->eval(valorLinea);
+
+	  if(ins){
+	    cout << "=> " << result << endl;
+	  }
+	}catch(Exception e){
+	  cout << "* parse error" << endl;
+	}      
+      }
+      cont++;
+    }
+  }else{
+    while(cout << ">"){  
       try{
-	string buffer2(buffer);
-	istringstream ins(buffer2);
+	//cout << "Please enter a calculator expression: ";
+    
+	getline(cin, line);
+	// line + '\n';
 
-	string valorLinea;
-	ins >> valorLinea;
+	//calc = new Calculator();
 
-	result = calc->eval(valorLinea);
-
-	if(ins){
-	  cout << result << endl;
+	if(cin.eof()){
+	  break;
 	}
+
+	result = calc->eval(line);
+
+	//cout << "The result is " << result << endl;
+
+	//cout << "Antes del result" << endl;
+	cout << "=> " << result << endl;
+	//cout << "Despues del result" << endl;
+
+	//delete calc;
+    
       }catch(Exception e){
 	cout << "* parse error" << endl;
-      }      
-    }
-    cont++;
-  }
-  while(cout << ">"){  
-    try{
-      //cout << "Please enter a calculator expression: ";
-    
-      getline(cin, line);
-      // line + '\n';
-
-      //calc = new Calculator();
-
-      if(cin.eof()){
-	break;
       }
-
-      result = calc->eval(line);
-
-      //cout << "The result is " << result << endl;
-
-      cout << "Antes del result" << endl;
-      cout << result << endl;
-      cout << "Despues del result" << endl;
-
-      //delete calc;
-    
-    }catch(Exception e){
-      cout << "* parse error" << endl;
     }
   }
   delete calc;  
